e5416721a7690c9f3cfa7f86f2b5a30535b6784a
Vito Maiocchi <vito.maiocchi@outlook.com>
2024-12-17 13:19:07 +0100

    XCB atoms: check for xwayland popups

--- surface.cpp
+++ surface.cpp
@@ -7,6 +7,9 @@
 
 #include <wlr/util/log.h>
 #include <cassert>
+#include <map>
+#include <iostream>
+#include <set>
 
 namespace Surface {
 	//BASE CLASS
@@ -157,6 +160,7 @@ namespace Surface {
 	wl_listener new_xdg_toplevel;
 	wlr_xdg_decoration_manager_v1 *xdg_decoration_mgr;
 	wl_listener xdg_decoration_listener;
+	wl_listener new_xdg_popup;
 
 	struct xdg_shell_surface_listeners {
 		XdgToplevel* surface;
@@ -171,6 +175,8 @@ namespace Surface {
 		wlr_xdg_toplevel* xdg_toplevel = (wlr_xdg_toplevel*) data;
 		xdg_shell_surface_listeners* listeners = new xdg_shell_surface_listeners();
 		listeners->surface = new XdgToplevel(xdg_toplevel);
+		xdg_toplevel->base->surface->data = listeners->surface;
+		wlr_log(WLR_DEBUG, "new xdg toplevel %p", listeners->surface);
 
 				//CONFIGURE LISTENERS
 		listeners->map_listener.notify = [](struct wl_listener* listener, void* data) {
@@ -202,6 +208,17 @@ namespace Surface {
 		wl_signal_add(&xdg_toplevel->base->surface->events.destroy, 	&listeners->destroy_listener);
 	}
 
+	void new_xdg_popup_notify(struct wl_listener* listener, void* data) {
+		wlr_xdg_popup* xdg_popup = (wlr_xdg_popup*) data;
+		if(!xdg_popup->parent) {
+			wlr_log(WLR_DEBUG, "XDG toplevel new popup: no parent");
+			return;
+		}
+		Base* surface = (Base*) xdg_popup->parent->data;
+		wlr_log(WLR_DEBUG, "XDG toplevel new popup with parent: %p", surface);
+		//TODO create xdg popup
+	}
+
 	void xdg_new_decoration_notify(struct wl_listener *listener, void *data) {
 		struct wlr_xdg_toplevel_decoration_v1 *dec = (wlr_xdg_toplevel_decoration_v1*) data;
 		wlr_xdg_toplevel_decoration_v1_set_mode(dec, WLR_XDG_TOPLEVEL_DECORATION_V1_MODE_SERVER_SIDE);
@@ -217,21 +234,9 @@ namespace Surface {
 			root_node = wlr_scene_tree_create(&Output::scene->tree);
 		}
 
-		void associate(bool associated) {
-			this->associated = associated;
-			if(associated && waiting_for_map) {
-				surface_node = wlr_scene_subsurface_tree_create(root_node, xwayland_surface->surface);
-				mapNotify(true);
-			}
-			if(!associated) mapNotify(false);
-		}
-
-		void map_request() {
-			waiting_for_map = true;
-			if(associated && waiting_for_map) {
-				surface_node = wlr_scene_subsurface_tree_create(root_node, xwayland_surface->surface);
-				mapNotify(true);
-			}
+		void map(bool map) {
+			if(map) surface_node = wlr_scene_subsurface_tree_create(root_node, xwayland_surface->surface);
+			mapNotify(map);
 		}
 
         ~XwaylandToplevel() {
@@ -239,9 +244,6 @@ namespace Surface {
 		}
 
         private:
-		bool waiting_for_map = false;
-		bool associated = false;
-
         void setSurfaceSize(uint width, uint height) {
 			wlr_xwayland_surface_configure(xwayland_surface, 0,0, width, height);
 		}
@@ -260,38 +262,97 @@ namespace Surface {
 	wl_listener new_xwayland_surface;
 	wl_listener xwayland_ready_listener;
 
+	enum XWaylandWindowType {
+		XWAYLAND_UNASSOCIATED,
+		XWAYLAND_TOPLEVEL,
+		XWAYLAND_POPUP,
+	};
+
+	XWaylandWindowType getXWaylandWindowType(wlr_xwayland_surface* surface);
+
 	struct xwayland_surface_listeners {
-		XwaylandToplevel* surface;
+		union {
+			XwaylandToplevel* surface;
+		};
 		wl_listener map;
 		wl_listener associate;
 		wl_listener disassociate;
 		wl_listener destroy;
+
+		bool mapped = false;
+
+		wlr_xwayland_surface* wlr_surface;
+		XWaylandWindowType type = XWAYLAND_UNASSOCIATED;
+
+		void tryMap() {
+			assert(type == XWAYLAND_TOPLEVEL);
+			if(!mapped) return;
+			surface->map(true);
+		}
 	};
 
 	void new_xwayland_surface_notify(struct wl_listener* listener, void* data) {
 		debug("NEW XWAYLAND SURFACE NOTIFY");
-		struct wlr_xwayland_surface* surface = (wlr_xwayland_surface*) data;
+		wlr_xwayland_surface* surface = (wlr_xwayland_surface*) data;
 		auto listeners = new xwayland_surface_listeners;
-		listeners->surface = new XwaylandToplevel(surface);
+		listeners->wlr_surface = surface;
 
 		listeners->associate.notify = [](struct wl_listener* listener, void* data) {
 			xwayland_surface_listeners* listeners = wl_container_of(listener, listeners, associate);
+			listeners->type = getXWaylandWindowType(listeners->wlr_surface);
+
 			wlr_log(WLR_DEBUG, "surface %p is now associated", listeners->surface);
-			listeners->surface->associate(true);
+			switch(listeners->type) {
+				case XWAYLAND_TOPLEVEL:
+					debug("new xwayland toplevel");
+					listeners->surface = new XwaylandToplevel(listeners->wlr_surface);
+					listeners->tryMap();
+				break;
+				case XWAYLAND_POPUP:
+					debug("new xwayland popup");
+					//TODO
+				break;
+				default:
+				debug("xwayland surface of unknown type");
+				break;
+			}
 		};
 		wl_signal_add(&surface->events.associate, &listeners->associate);
 
 		listeners->map.notify = [](struct wl_listener* listener, void* data) {
 			xwayland_surface_listeners* listeners = wl_container_of(listener, listeners, map);
 			wlr_log(WLR_DEBUG, "surface %p wants to be mapped", listeners->surface);
-			listeners->surface->map_request();
+			listeners->mapped = true;
+
+			switch(listeners->type) {
+				case XWAYLAND_TOPLEVEL:
+					debug("map xwayland toplevel");
+					listeners->tryMap();
+				break;
+				default:
+				debug("unhandled map case");
+				break;
+			}
 		};
 		wl_signal_add(&surface->events.map_request, &listeners->map);
 
 		listeners->disassociate.notify = [](struct wl_listener* listener, void* data) {
 			xwayland_surface_listeners* listeners = wl_container_of(listener, listeners, disassociate);
 			wlr_log(WLR_DEBUG, "surface %p disassociated", listeners->surface);
-			listeners->surface->associate(false);
+
+			switch(listeners->type) {
+				case XWAYLAND_TOPLEVEL:
+					debug("disassociate xwayland toplevel");
+					listeners->surface->map(false);
+				break;
+				case XWAYLAND_POPUP:
+					debug("disassociate xwayland popup");
+					//TODO
+				break;
+				default:
+				debug("xwayland surface of unknown type");
+				break;
+			}
 		};
 		wl_signal_add(&surface->events.dissociate, &listeners->disassociate);
 
@@ -299,7 +360,19 @@ namespace Surface {
 			xwayland_surface_listeners* listeners = wl_container_of(listener, listeners, destroy);
 			wlr_log(WLR_DEBUG, "surface %p destroyed", listeners->surface);
 
-			delete listeners->surface;
+			switch(listeners->type) {
+				case XWAYLAND_TOPLEVEL:
+					debug("destroy xwayland toplevel");
+					delete listeners->surface;
+				break;
+				case XWAYLAND_POPUP:
+					debug("destroy xwayland popup");
+					//TODO
+				break;
+				default:
+				debug("xwayland surface of unknown type");
+				break;
+			}
 
 			wl_list_remove(&listeners->map.link);
 			wl_list_remove(&listeners->associate.link);
@@ -311,20 +384,7 @@ namespace Surface {
 		wl_signal_add(&surface->events.destroy, &listeners->destroy);
 	}
 
-	//kein plan was das macht
-	enum { NetWMWindowTypeDialog, NetWMWindowTypeSplash, NetWMWindowTypeToolbar,
-	NetWMWindowTypeUtility, NetLast }; /* EWMH atoms */
-	static xcb_atom_t netatom[NetLast];
-	xcb_atom_t getatom(xcb_connection_t *xc, const char *name) {
-		xcb_atom_t atom = 0;
-		xcb_intern_atom_reply_t *reply;
-		xcb_intern_atom_cookie_t cookie = xcb_intern_atom(xc, 0, strlen(name), name);
-		if ((reply = xcb_intern_atom_reply(xc, cookie, NULL)))
-			atom = reply->atom;
-		free(reply);
-
-		return atom;
-	}
+	void fetch_atoms(xcb_connection_t* xc);
 
 	void xwayland_ready(struct wl_listener* listener, void* data) {
 		debug("X WAYLAND READY");
@@ -336,17 +396,12 @@ namespace Surface {
 			return;
 		}
 
-		/* Collect atoms we are interested in. If getatom returns 0, we will
-		* not detect that window type. */
-		netatom[NetWMWindowTypeDialog] = getatom(xc, "_NET_WM_WINDOW_TYPE_DIALOG");
-		netatom[NetWMWindowTypeSplash] = getatom(xc, "_NET_WM_WINDOW_TYPE_SPLASH");
-		netatom[NetWMWindowTypeToolbar] = getatom(xc, "_NET_WM_WINDOW_TYPE_TOOLBAR");
-		netatom[NetWMWindowTypeUtility] = getatom(xc, "_NET_WM_WINDOW_TYPE_UTILITY");
+		fetch_atoms(xc);
 
-		/* assign the one and only seat */
+		// assign the one and only seat
 		wlr_xwayland_set_seat(xwayland, Input::seat);
 
-		/* Set the default XWayland cursor to match the rest of dwl. */
+		//Set the default XWayland cursor.
 		if ((xcursor = wlr_xcursor_manager_get_xcursor(Input::cursor_mgr, "default", 1)))
 			wlr_xwayland_set_cursor(xwayland,
 					xcursor->images[0]->buffer, xcursor->images[0]->width * 4,
@@ -361,7 +416,9 @@ namespace Surface {
 
 		xdg_shell = wlr_xdg_shell_create(Server::display, 3);
 		new_xdg_toplevel.notify = new_xdg_toplevel_notify;
+		new_xdg_popup.notify = new_xdg_popup_notify;
 		wl_signal_add(&xdg_shell->events.new_toplevel, &new_xdg_toplevel);	
+		wl_signal_add(&xdg_shell->events.new_popup, &new_xdg_popup);
 
 		xwayland = wlr_xwayland_create(Server::display, Server::compositor, false);
 		
@@ -378,67 +435,82 @@ namespace Surface {
 		xdg_decoration_mgr = wlr_xdg_decoration_manager_v1_create(Server::display);
 		xdg_decoration_listener.notify = xdg_new_decoration_notify;
 		wl_signal_add(&xdg_decoration_mgr->events.new_toplevel_decoration, &xdg_decoration_listener);
-
-		//TODO: DA MÜND NO POPUPS GHANDLED WERDE
-		/*
-		if (xdg_surface->role == WLR_XDG_SURFACE_ROLE_POPUP) {
-			//parent data = the wlr_scene_tree of parent
-			struct wlr_xdg_surface *parent = wlr_xdg_surface_try_from_wlr_surface(xdg_surface->popup->parent);
-			assert(parent != NULL);
-			xdg_surface->data = wlr_scene_xdg_surface_create(parent->data, xdg_surface);
-			return;
-		}
-		*/
-
-		//TODO: LAYER SHELL IMPLEMENT das isch für so rofi und so
-		/*
-		server.layer_shell = wlr_layer_shell_v1_create(server.display, 4);
-		server.new_layer_shell_surface.notify = layer_shell_new_surface;
-		server.delete_layer_shell_surface.notify = layer_shell_delete_surface;
-		wl_signal_add(&server.layer_shell->events.new_surface, &server.new_layer_shell_surface);
-		//FIXME: DELTE isch glaub wenn LAYERSHELL DELTED WIRD nöd die SURFACE
-		wl_signal_add(&server.layer_shell->events.destroy, &server.delete_layer_shell_surface);
-		wl_list_init(&lss_list);
-		//server.layer_shell->events.new_surface
-		*/
 	}
 
 
-	//LAYER SHELL ULTRA BASIC
-	/*
-	struct LayerShellSurface {
-		struct wl_list link;
-		struct wlr_layer_surface_v1* ls_surface; 
-		struct wlr_scene_layer_surface_v1* surface_node;
+	//XCB ATOM STUFF
+	//TODO: clean up this code
+	enum Atom {
+		NET_WM_WINDOW_TYPE_NORMAL,
+		NET_WM_WINDOW_TYPE_DIALOG,
+		NET_WM_WINDOW_TYPE_UTILITY,
+		NET_WM_WINDOW_TYPE_TOOLBAR,
+		NET_WM_WINDOW_TYPE_SPLASH,
+		NET_WM_WINDOW_TYPE_MENU,
+		NET_WM_WINDOW_TYPE_DROPDOWN_MENU,
+		NET_WM_WINDOW_TYPE_POPUP_MENU,
+		NET_WM_WINDOW_TYPE_TOOLTIP,
+		NET_WM_WINDOW_TYPE_NOTIFICATION,
+		NET_WM_STATE_MODAL,
+		ATOM_LAST,
 	};
 
-	struct wl_list lss_list;
+	const char* atom_string[ATOM_LAST] = {
+		[NET_WM_WINDOW_TYPE_NORMAL] 		= "_NET_WM_WINDOW_TYPE_NORMAL",
+		[NET_WM_WINDOW_TYPE_DIALOG] 		= "_NET_WM_WINDOW_TYPE_DIALOG",
+		[NET_WM_WINDOW_TYPE_UTILITY] 		= "_NET_WM_WINDOW_TYPE_UTILITY",
+		[NET_WM_WINDOW_TYPE_TOOLBAR] 		= "_NET_WM_WINDOW_TYPE_TOOLBAR",
+		[NET_WM_WINDOW_TYPE_SPLASH] 		= "_NET_WM_WINDOW_TYPE_SPLASH",
+		[NET_WM_WINDOW_TYPE_MENU] 			= "_NET_WM_WINDOW_TYPE_MENU",
+		[NET_WM_WINDOW_TYPE_DROPDOWN_MENU] 	= "_NET_WM_WINDOW_TYPE_DROPDOWN_MENU",
+		[NET_WM_WINDOW_TYPE_POPUP_MENU] 	= "_NET_WM_WINDOW_TYPE_POPUP_MENU",
+		[NET_WM_WINDOW_TYPE_TOOLTIP] 		= "_NET_WM_WINDOW_TYPE_TOOLTIP",
+		[NET_WM_WINDOW_TYPE_NOTIFICATION] 	= "_NET_WM_WINDOW_TYPE_NOTIFICATION",
+		[NET_WM_STATE_MODAL] 				= "_NET_WM_STATE_MODAL",
+	};
 
-	void layer_shell_new_surface(struct wl_listener *listener, void *data) {
-		struct wlr_layer_surface_v1* surface = (wlr_layer_surface_v1*) data;
-		if(!surface) return;
-
-		struct LayerShellSurface* lss = (LayerShellSurface* ) calloc(1, sizeof(struct LayerShellSurface));
-		wl_list_insert(&lss_list, &lss->link);
-		lss->ls_surface = surface;
-
-		struct wlr_output* output = surface->output;
-		if(!output) {
-			if (wl_list_empty(&server.outputs)) return;
-			struct WaylandOutput* wlo = wl_container_of(server.outputs.next, wlo, link);
-			output = wlo->wlr_output;
-		}
-
-		lss->surface_node = wlr_scene_layer_surface_v1_create(&server.scene->tree, surface);
-		//FIXME: ULTRA JANKY (0,0) ich muss für das zerscht d outputs besser mache
-		wlr_scene_node_set_position(&lss->surface_node->tree->node, 0, 0);
-		wlr_layer_surface_v1_configure(surface, output->width, output->height);
-
-		//TODO: FREE LAYERSHELL eventually -> memory leak
-	}
-
-	void layer_shell_delete_surface(struct wl_listener *listener, void *data) {
-		//idk ob ich da was will
-	}
+	/*
+	die gis au no 
+	"_NET_WM_WINDOW_TYPE_DESKTOP",
+    "_NET_WM_WINDOW_TYPE_DOCK",
+    "_NET_WM_WINDOW_TYPE_COMBO",
+    "_NET_WM_WINDOW_TYPE_DND",
 	*/
+
+
+	std::map<xcb_atom_t, Atom> atoms;
+
+	std::set<Atom> getAtoms(xcb_atom_t* atom_array, size_t size) {
+		std::set<Atom> a;
+		for(uint i = 0; i < size; i++) {
+			auto it = atoms.find(atom_array[i]);
+			a.insert(it->second);
+		}
+		return a;
+	}
+
+	void printSet(std::set<Atom> atom) {
+		std::cout << "Atoms len:" << atom.size() << std::endl;
+		for(auto a : atom) std::cout << atom_string[a] << std::endl;
+	}
+
+
+	XWaylandWindowType getXWaylandWindowType(wlr_xwayland_surface* surface) {
+		std::set<Atom> a = getAtoms(surface->window_type, surface->window_type_len);
+		printSet(a); //debug
+		if(a.find(NET_WM_WINDOW_TYPE_POPUP_MENU) != a.end()) return XWAYLAND_POPUP;
+		return XWAYLAND_TOPLEVEL;
+	}
+
+	void fetch_atoms(xcb_connection_t* xc) {
+		for(Atom atom = (Atom) 0; atom < ATOM_LAST; atom = static_cast<Atom>(atom + 1) ) {
+			const auto name = atom_string[atom];
+			xcb_intern_atom_reply_t *reply;
+			xcb_intern_atom_cookie_t cookie = xcb_intern_atom(xc, 0, strlen(name), name);
+			if ((reply = xcb_intern_atom_reply(xc, cookie, NULL)))
+				atoms[reply->atom] = atom;
+			free(reply);
+		}
+	}
+
 }
