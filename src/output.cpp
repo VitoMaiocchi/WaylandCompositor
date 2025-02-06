1efc3dc2066ef7faf9cf4fcf3cc45406cfbcf769
Vito Maiocchi <vito.maiocchi@outlook.com>
2025-02-06 18:53:29 +0100

    PulseAudio -> Pipewire 02: cleanup

--- src/output.cpp
+++ src/output.cpp
@@ -122,8 +122,8 @@ namespace Output {
 		wl_signal_add(&wlr_output->events.destroy, &listeners->destroy);
 	}
 
-	void setupPulseAudio();
-	void cleanupPulseAudio();
+	void setupPipeWire();
+	//TODO: cleanup Pipewire
 
     void setup() {
         output_layout = wlr_output_layout_create(Server::display);
@@ -134,14 +134,14 @@ namespace Output {
         scene = wlr_scene_create();
         scene_layout = wlr_scene_attach_output_layout(scene, output_layout);
 
-		setupPulseAudio();
+		setupPipeWire();
     }
 
     void cleanup() {
         wlr_scene_node_destroy(&scene->tree.node);
         wlr_output_layout_destroy(output_layout);
 
-		cleanupPulseAudio();
+		//cleanup pipewire
     }
 
 	//MONITOR END
@@ -228,237 +228,179 @@ namespace Output {
 		wlr_buffer_drop(&buffer->base);
 	}
 
-	pa_threaded_mainloop* mainloop;
-	pa_context* context;
-	pa_volume_t current_volume = PA_VOLUME_INVALID;
-
-	std::atomic<bool> connected = false;
-	const pa_sink_info* default_sink = nullptr;
-
-	void adjustVolume(double vol) {
-		assert(vol < 1 && vol > -1);
-		if(!connected) return;
-		if(!default_sink) return;
-
-		pa_threaded_mainloop_lock(mainloop);
-
-		pa_cvolume new_vol;
-		new_vol.channels = default_sink->volume.channels;
-		debug("default channels: {}", default_sink->volume.channels);
-		uint max = PA_CHANNELS_MAX < new_vol.channels ? PA_CHANNELS_MAX : new_vol.channels;
-		debug("max={}",max);
-		for(uint i = 0; i < max; i++) {
-			pa_volume_t o = default_sink->volume.values[i];
-			pa_volume_t n;
-			if(vol > 0) {
-				if(o + PA_VOLUME_NORM*vol > PA_VOLUME_NORM) n = PA_VOLUME_NORM;
-				else n = o + PA_VOLUME_NORM*vol;
-			} else {
-				if(o < PA_VOLUME_NORM*(-vol)) n = 0;
-				else n = o - PA_VOLUME_NORM*(-vol);
-			}
-			new_vol.values[i] = n;
-		}
-		pa_context_set_sink_volume_by_index(context, default_sink->index, &new_vol, nullptr, nullptr);
-		//TODO: das mit by name und nöd by index mache den muss mer de sink nöd speichere pa_context_set_sink_volume_by_name()
-		pa_threaded_mainloop_unlock(mainloop);
-		debug("KYS");
-	}
-
-	void synchronized_volume_update(void* data) {
-		const pa_volume_t volume = *(pa_volume_t*)data;
-		delete (pa_volume_t*)data;
-
-		const uint vol_percent = std::round( (float)volume*100/PA_VOLUME_NORM );
-		Titlebar::updateVolume(vol_percent);
-	}
-
-	//TODO: add support for mute
-	void get_sink_volume_callback(pa_context *c, const pa_sink_info *i, int is_last, void *userdata) {
-		if(is_last) return;
-		assert(i);
-		default_sink = i;
-		const pa_volume_t volume = pa_cvolume_avg(&i->volume);
-		if(volume == current_volume) return;
-		current_volume = volume;
-		Server::queueEvent(synchronized_volume_update, new pa_volume_t(current_volume));
-	}
 
 
-	void subscribe_callback(pa_context *c, pa_subscription_event_type_t t, uint32_t index, void *userdata) {
-		if ((t & PA_SUBSCRIPTION_EVENT_FACILITY_MASK) == PA_SUBSCRIPTION_EVENT_SINK &&
-			(t & PA_SUBSCRIPTION_EVENT_TYPE_MASK) == PA_SUBSCRIPTION_EVENT_CHANGE) {
-			pa_context_get_sink_info_by_name(c, "@DEFAULT_SINK@", get_sink_volume_callback, nullptr);
-		}
-	}
 
-	GOptionContext* gcontext;
-	GMainLoop* loop;
-	WpCore* core;
-	WpObjectManager* om;
-	uint pending_plugins = 0;
+	// void synchronized_volume_update(void* data) {
+	// 	const pa_volume_t volume = *(pa_volume_t*)data;
+	// 	delete (pa_volume_t*)data;
 
-	void default_node_update_notify(GObject *emitter, guint arg, gpointer user_data) {
-		debug("DEFAULT NODE UPDATE");
-	}
+	// 	const uint vol_percent = std::round( (float)volume*100/PA_VOLUME_NORM );
+	// 	Titlebar::updateVolume(vol_percent);
+	// }
 
-	void mixer_update_notify(GObject *emitter, guint arg, gpointer user_data) {
-		debug("VOLUME CHANGE (MIXER) UPDATE");
-	}
+	// void get_sink_volume_callback(pa_context *c, const pa_sink_info *i, int is_last, void *userdata) {
+	// 	if(is_last) return;
+	// 	assert(i);
+	// 	default_sink = i;
+	// 	const pa_volume_t volume = pa_cvolume_avg(&i->volume);
+	// 	if(volume == current_volume) return;
+	// 	current_volume = volume;
+	// 	Server::queueEvent(synchronized_volume_update, new pa_volume_t(current_volume));
+	// }
 
-	static void on_plugin_loaded (WpCore * core, GAsyncResult * res, void* data) {
-		GError *error = NULL;
 
-		if (!wp_core_load_component_finish (core, res, &error)) {
-			fprintf (stderr, "%s\n", error->message);
-			error("error loading pipewire/wireplumber plugin");
-			g_main_loop_quit (loop);
-			return;
-		}
 
-		if (--pending_plugins == 0) {
-			auto mixer_api = wp_plugin_find (core, "mixer-api");
-			g_object_set (mixer_api, "scale", 1 /* cubic */, NULL);
-			wp_core_install_object_manager (core, om);
-		}
-	}
+	//PIPEWIRE / WIREPLUMBER
 
 	#define MAXINT32	(0x7fffffff)
 	#define MAXUINT32	(0xffffffff)
 
+	std::atomic<bool> mainloop_running = false;
+	std::atomic<bool> mute_toggle = false;
+	std::atomic<double> volume_change = 0.0;
+
+	void adjustVolume(double vol) {
+		//kys
+	}
+
+	struct WpData {
+		GOptionContext* gcontext;
+		GMainLoop* loop;
+		WpCore* core;
+		WpObjectManager* om;
+		uint pending_plugins = 0;
+
+		WpPlugin* def_nodes_api = nullptr;
+		WpPlugin* mixer_api = nullptr;
+
+		struct {
+			double volume = 1.0;
+			bool muted = false;
+			uint32_t id = 0;
+		} defaultSink;
+	};
+
 	int32_t get_default_sink_id(WpPlugin* def_nodes_api) {
 		int32_t res = -1;
-
 		g_signal_emit_by_name (def_nodes_api, "get-default-node", "Audio/Sink", &res); //"Audio/Source" für source
 		if(res <= 0 || res >= MAXUINT32) error("default node id is not valid");
-
 		return res;
 	}
 
-
-	void do_print_volume (gpointer proxy) {
-		WpPlugin* mixer_api = wp_plugin_find (core, "mixer-api");
+	void update_volume(WpData* wpd) {
 		GVariant *variant = NULL;
 		gboolean mute = FALSE;
 		gdouble volume = 1.0;
-		int32_t id = wp_proxy_get_bound_id (WP_PROXY (proxy));
 
-	
-		g_signal_emit_by_name (mixer_api, "get-volume", id, &variant);
+		g_signal_emit_by_name (wpd->mixer_api, "get-volume", wpd->defaultSink.id, &variant);
 		if (!variant) {
-			fprintf (stderr, "Node %d does not support volume\n", id);
+			error("Node {} does not support volume", wpd->defaultSink.id);
 			return;
 		}
+
 		g_variant_lookup (variant, "volume", "d", &volume);
 		g_variant_lookup (variant, "mute", "b", &mute);
 		g_clear_pointer (&variant, g_variant_unref);
 
-		printf ("Volume: %.2f%s", volume, mute ? " [MUTED]\n" : "\n");
+
+		if(volume != wpd->defaultSink.volume) {
+			debug("volume updated: volume={:.2f}", volume);
+			wpd->defaultSink.volume = volume;
+		}
+
+		if(mute != wpd->defaultSink.muted) {
+			debug("audio mute status updated: mute={}", mute);
+			wpd->defaultSink.muted = mute;
+		}
 	}
 
-	void state_changed_callback (WpNode * self, WpNodeState old_state, WpNodeState new_state, gpointer user_data) {
-		debug("STATE CHANGED");
+	void default_node_update_notify(GObject *emitter, guint arg, WpData* wpd) {
+		uint32_t id = get_default_sink_id(wpd->def_nodes_api);
+		if(id != wpd->defaultSink.id) {
+			debug("default audio node changed: new_id={}",id);
+			wpd->defaultSink.id = id;
+			update_volume(wpd);
+		}
 	}
 
-	void get_volume_run(void* data) {
-		WpPlugin* def_nodes_api = NULL;
-		GError* error = NULL;
-		gpointer proxy = NULL; // (WpPipewireObject*)
-		int32_t id;
+	void mixer_update_notify(GObject *emitter, guint arg, WpData* wpd) {
+		update_volume(wpd);
+	}
 
-		def_nodes_api = wp_plugin_find (core, "default-nodes-api");
-		id = get_default_sink_id(def_nodes_api);
+	void get_volume_run(WpData* wpd) {
+		if(!wpd->def_nodes_api) error("defnodes not loaded");
+		if(!wpd->mixer_api) error("mixer not loaded");
+		
+		wpd->defaultSink.id = get_default_sink_id(wpd->def_nodes_api);
+		update_volume(wpd);
 
-		proxy = wp_object_manager_lookup (om, WP_TYPE_GLOBAL_PROXY,
-			WP_CONSTRAINT_TYPE_G_PROPERTY, "bound-id", "=u", id, NULL);
+		g_signal_connect(wpd->def_nodes_api, "changed", G_CALLBACK(default_node_update_notify), wpd);
+		g_signal_connect(wpd->mixer_api, "changed", G_CALLBACK(mixer_update_notify), wpd);  
+	}
 
-		if (!proxy) {
-			fprintf (stderr, "Node '%d' not found\n", id);
+	void wireplumber_disconnect_notify(void* data) {
+		warn("wireplumber disconnected");
+		mainloop_running = false;
+	}
+
+	void on_plugin_loaded (WpCore * core, GAsyncResult * res, WpData* wpd) {
+		GError *error = NULL;
+		if (!wp_core_load_component_finish (core, res, &error)) {
+			error("error loading wireplumber plugin errormessage=\"{}\"", error->message);
+			mainloop_running = false;
 			return;
 		}
 
-		g_signal_connect(
-		proxy,         // The Wireplumber object (e.g., WpNode, WpGlobalProxy)
-		"state-changed",  // The name of the signal (e.g., "notify", "volume-changed")
-		G_CALLBACK(state_changed_callback),  // Your callback
-		nullptr     // Optional data passed to the callback
-		);
-
-		do_print_volume(proxy);
-
-		WpPlugin* mixer_api = wp_plugin_find (core, "mixer-api");
-
-		g_signal_connect(def_nodes_api, "changed", G_CALLBACK(default_node_update_notify), nullptr);
-		g_signal_connect(mixer_api, "changed", G_CALLBACK(mixer_update_notify), nullptr);  
-
+		if (--wpd->pending_plugins == 0) {
+			info("wireplumber plugins loaded");
+			wpd->def_nodes_api = wp_plugin_find(core, "default-nodes-api");
+			wpd->mixer_api  = wp_plugin_find(core, "mixer-api");
+			g_object_set (wpd->mixer_api, "scale", 1 /* cubic */, NULL);
+			wp_core_install_object_manager (core, wpd->om);
+		}
 	}
 
-	static gboolean get_volume_prepare() {
-		wp_object_manager_add_interest (om, WP_TYPE_NODE, NULL);
-		wp_object_manager_request_object_features (om, WP_TYPE_GLOBAL_PROXY,
-			WP_PIPEWIRE_OBJECT_FEATURES_MINIMAL);
-		return TRUE;
-	}
+	void wireplumber_run_mainloop() {
+		WpData wpd;
 
-	void setupPipeWire() {
 		wp_init (WP_INIT_ALL);
-		gcontext = g_option_context_new ("Vito Manager");
-		loop = g_main_loop_new (NULL, FALSE);
-		core = wp_core_new (NULL, NULL, NULL);
-		om = wp_object_manager_new ();
+		wpd.gcontext = g_option_context_new ("Vito Manager");
+		wpd.loop = g_main_loop_new (NULL, FALSE);
+		wpd.core = wp_core_new (NULL, NULL, NULL);
+		wpd.om = wp_object_manager_new ();
 
-		get_volume_prepare();
+		wp_object_manager_add_interest (wpd.om, WP_TYPE_NODE, NULL);
+		wp_object_manager_request_object_features (wpd.om, WP_TYPE_GLOBAL_PROXY, WP_PIPEWIRE_OBJECT_FEATURES_MINIMAL);
 
 		 /* load required API modules */
-		pending_plugins++;
-		wp_core_load_component (core, "libwireplumber-module-default-nodes-api",
-			"module", NULL, NULL, NULL, (GAsyncReadyCallback) on_plugin_loaded, nullptr);
-		pending_plugins++;
-		wp_core_load_component (core, "libwireplumber-module-mixer-api",
-			"module", NULL, NULL, NULL, (GAsyncReadyCallback) on_plugin_loaded, nullptr);
+		wpd.pending_plugins++;
+		wp_core_load_component (wpd.core, "libwireplumber-module-default-nodes-api",
+			"module", NULL, NULL, NULL, (GAsyncReadyCallback) on_plugin_loaded, &wpd);
+		wpd.pending_plugins++;
+		wp_core_load_component (wpd.core, "libwireplumber-module-mixer-api",
+			"module", NULL, NULL, NULL, (GAsyncReadyCallback) on_plugin_loaded, &wpd);
 
 		/* connect */
-		if (!wp_core_connect (core)) {
+		if (!wp_core_connect (wpd.core)) {
 			fprintf (stderr, "Could not connect to PipeWire\n");
 			error("Could not connect to PipeWire");
 			return;
 		}
 
 		/* run */
-		g_signal_connect_swapped (core, "disconnected",
-			(GCallback) g_main_loop_quit, loop);
-		g_signal_connect_swapped (om, "installed",
-			(GCallback) get_volume_run, nullptr);
+		g_signal_connect_swapped (wpd.core, "disconnected",
+			(GCallback) wireplumber_disconnect_notify, nullptr);
+		g_signal_connect_swapped (wpd.om, "installed",
+			(GCallback) get_volume_run, &wpd);
 
-		while(true) g_main_context_iteration(g_main_loop_get_context(loop), true);
-		debug("kys");
+		mainloop_running = true;
+		while(mainloop_running) g_main_context_iteration(g_main_loop_get_context(wpd.loop), true);
+		info("wireplumber mainloop stopped");
 	}
 
-	void setupPulseAudio() {
-		setupPipeWire();
-
-		mainloop = pa_threaded_mainloop_new();
-		pa_mainloop_api *mainloop_api = pa_threaded_mainloop_get_api(mainloop);
-		context = pa_context_new(mainloop_api, "Vito Manager");
-		pa_context_connect(context, nullptr, PA_CONTEXT_NOAUTOSPAWN, nullptr);
-
-		pa_context_set_state_callback(context, [](pa_context *c, void *userdata) {
-			if (pa_context_get_state(c) == PA_CONTEXT_READY) {
-				connected = true;
-				debug("pulse ready");
-				pa_context_get_sink_info_by_name(c, "@DEFAULT_SINK@", get_sink_volume_callback, nullptr);
-				pa_context_set_subscribe_callback(c, subscribe_callback, nullptr);
-				pa_context_subscribe(c, PA_SUBSCRIPTION_MASK_SINK, nullptr, nullptr);
-			}
-		}, nullptr);
-
-		pa_threaded_mainloop_start(mainloop);
+	void setupPipeWire() {
+		wireplumber_run_mainloop();
 	}
 
-	void cleanupPulseAudio() {
-		connected = false;
-		pa_context_unref(context);
-		pa_threaded_mainloop_stop(mainloop);
-		pa_threaded_mainloop_free(mainloop);
-	}
 }
